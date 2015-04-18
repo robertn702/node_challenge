bd16caf4cca160ab42a78471c4ff31d416928b2a
Robert Niimi <Robert Niimi>
2015-04-18 16:14:35 -0700

    allows users to check/uncheck and save list

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,17 +3,25 @@ angular.module('app.controllers', [])
 .controller('newListController', function($scope, List) {
   $scope.list = List.get() || [];
   console.log('list: ', $scope.list);
+
   $scope.addItem = function(newItem) {
     $scope.newItem = "";
     $scope.list.push({
+      id: $scope.list.length,
       date: new Date(),
-      text: newItem
+      text: newItem,
+      complete: false
     })
   };
+
   $scope.save = function() {
+    console.log('selected: ', $scope.list);
+    // console.log(angular.element(document.querySelectorAll('.list-item')).prop('checked'));
     List.post($scope.list)
   };
 })
+
+
 .controller('listController', function($scope, List) {
   $scope.list = List.get();
 })
